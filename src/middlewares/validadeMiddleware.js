b2a4e902809d6bd91e172a07433cb2ea52ec9de3
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-02-08 04:08:14 -0300

    Games Schema

---
+++ src/middlewares/validadeMiddleware.js
@@ -0,0 +1,13 @@
+export function validateMiddleware(schema) {
+    return (req, res, next) => {
+        const {error} = schema.validate(req.body, {abortEarly: false})
+
+        if(error){
+            const errorMessages = error.details.map(err => err.message)
+            
+            return res.status(422).send(errorMessages)
+        }
+
+        next();
+    }
+}
