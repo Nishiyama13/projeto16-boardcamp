58c945a847234036d703040f4ad8c82367f9282c
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-02-12 21:06:07 -0300

    status change when there is no game available

--- src/controllers/RentalsControllers.js
+++ src/controllers/RentalsControllers.js
@@ -2,7 +2,7 @@
 import { db } from "../config/database.connection.js"
 import dayjs from "dayjs"
 
-/* export async function findAllRentals (req,res) {
+/*  export async function findAllRentals (req,res) {
     try{
         const rentals = await db.query("SELECT * FROM rentals");
         res.send(rentals.rows);
@@ -10,20 +10,22 @@ import dayjs from "dayjs"
     }catch(error){
         res.status(500).send(error.message);
     }
-} */
+}  */
 
 export async function createNewRentals (req,res){
     if(req.path !== "/rentals"){
-        return res.status(404).send("Endpoint não encontrado");
+        return res.status(400).send("Endpoint não encontrado");
     }
-    
+
     const { customerId, gameId, daysRented } = req.body;
 
     const rentalDate = dayjs(Date.now()).format("YYYY-MM-DD");
 
     try{
         const customer = await db.query('SELECT * FROM customers WHERE id = $1',[customerId]);
+
         const game = await db.query('SELECT * FROM games WHERE id=$1', [gameId]);
+        
         if(customer.rowCount === 0 || game.rowCount === 0){
             res.status(400).send("Cliente ou jogo não existe")
         }
@@ -35,7 +37,7 @@ export async function createNewRentals (req,res){
         const stock = await db.query(`SELECT "stockTotal" FROM games WHERE id = $1`,[gameId]);
 
         if(stock.rows[0].stockTotal <= rentals.rowCount){
-            res.status(201).send('Sem exemplar em estoque');
+            res.status(400).send('Sem exemplar em estoque');
         }
 
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *`, [customerId, gameId, rentalDate, daysRented, null, originalPrice, null]);
